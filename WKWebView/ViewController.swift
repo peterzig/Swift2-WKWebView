107e820ebc1cbda8e3f3c7e65b4729f9099f5e42
Peter Zaporowski <peter@Peters-Mac.local>
2017-03-09 22:30:56 +0100

    Using WKWebView to request some HTTP websites, WK stands for WebKit built-in browser

--- WKWebView/ViewController.swift
+++ WKWebView/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var container: UIView!
+    var webView: WKWebView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        webView = WKWebView()
+        container.addSubview(webView) // when code from viewDidAppear was here, constraints weren't good
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        let frame = CGRectMake(0, 0, container.bounds.width, container.bounds.height)
+        webView.frame = frame
+        
+        loadRequest("https://github.com/peterzig")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func loadRequest(urlString: String){
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL:url)
+        
+        webView.loadRequest(request)
+    }
+    
+    @IBAction func loadApple(sender: AnyObject) {
+        loadRequest("https://www.apple.com")
     }
 
+    @IBAction func loadGitHub(sender: AnyObject) {
+        loadRequest("https://github.com/peterzig")
+    }
 
+    @IBAction func loadAboutMe(sender: AnyObject) {
+        loadRequest("https://about.me/peterzig")
+    }
 }
-
